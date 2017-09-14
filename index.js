2c1c6947dc6044728ded9548e308a2abafb5421c
Ikbhal <iqbalforall@gmail.com>
2017-09-14 11:23:31 +0530

    Adding pug, layout

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/*
 app.get('/hello', function (req, res) {
    console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
-})
+   res.send('Hello GET1');
+});
+*/
+app.get('/faaiza1', function(req, res){
+  //res.send("Hello Faaiza");
+  res.render('index', {title:'Faaiza Cooking'});
+});
 
 var server = app.listen(8081, function () {
 
